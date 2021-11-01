41b2cc9d3ce7622eb9318421eece919acbc8c2fe
KaliAndriana <93439114+KaliAndriana@users.noreply.github.com>
2021-11-01 18:30:31 +0000

    Update server.go

--- api/server.go
+++ api/server.go
@@ -16,10 +16,10 @@ import (
 	"github.com/gorilla/mux"
 	"github.com/robfig/cron"
 
-	"github.com/btenterprise2020/open-etc-pool/payouts"
-	"github.com/btenterprise2020/open-etc-pool/rpc"
-	"github.com/btenterprise2020/open-etc-pool/storage"
-	"github.com/btenterprise2020/open-etc-pool/util"
+	"github.com/open-etc-pool/open-etc-pool/payouts"
+	"github.com/open-etc-pool/open-etc-pool/rpc"
+	"github.com/open-etc-pool/open-etc-pool/storage"
+	"github.com/open-etc-pool/open-etc-pool/util"
 )
 
 type ApiConfig struct {
